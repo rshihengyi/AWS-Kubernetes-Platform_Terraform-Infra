ac91fc544d483adb6c72ad5b46393da97d1eda01
Robert Heng <robertheng3@gmail.com>
2026-09-17 21:25:12 -0400

    Add files via upload

--- helm.tf
+++ helm.tf
@@ -2,12 +2,12 @@
     ArgoCD Helm Chart v9.5.9
 */
 
-#resource "kubernetes_namespace_v1" "argocd" {
-#  metadata {
-#    name = "argocd"
-#  }
-#  depends_on = [module.eks, aws_eks_access_entry.dev_sso_user]
-#}
+# resource "kubernetes_namespace_v1" "argocd" {
+#   metadata {
+#     name = "argocd"
+#   }
+#   depends_on = [module.eks, aws_eks_access_entry.dev_sso_user]
+# }
 
 resource "kubernetes_storage_class_v1" "grafana_storage_class" {
   metadata {
@@ -27,7 +27,7 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
   version    = "9.5.9"
-  namespace  = kubernetes_namespace_v1.argocd.metadata[0].name
+  namespace  = "argocd"
   depends_on = [module.eks, aws_eks_access_entry.dev_sso_user]
 }
 
