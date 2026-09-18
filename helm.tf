8bcba2a5712cba684da1c8bda5247d33860c816d
Robert Heng <robertheng3@gmail.com>
2026-09-17 20:50:40 -0400

    fix grafana storageclass

--- helm.tf
+++ helm.tf
@@ -11,13 +11,13 @@ resource "kubernetes_namespace_v1" "argocd" {
 
 resource "kubernetes_storage_class_v1" "grafana_storage_class" {
   metadata {
-    name = "monitoring"
+    name = "gp3"
   }
 
   storage_provisioner = "ebs.csi.aws.com"
   parameters = {
-    type                = "gp3"
-    volume-binding-mode = "WaitForFirstConsumer"
+    type              = "gp3"
+    VolumeBindingMode = "WaitForFirstConsumer"
   }
   depends_on = [module.eks, aws_eks_access_entry.dev_sso_user]
 }
