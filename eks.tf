828faff9b20d3cfad9d73f3161db4ee60074da36
Robert Heng <robertheng3@gmail.com>
2026-09-18 14:15:32 -0400

    upgrade t2small -> t3small

--- eks.tf
+++ eks.tf
@@ -40,7 +40,7 @@ module "eks" {
     node_group = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t2.small"] // CPU: 1   Mem: 2 GiB RAM
+      instance_types = ["t3.small"] // CPU: 1   Mem: 2 GiB RAM
 
       min_size     = 4 //total amount of running worker nodes spread across the subnets. 
       max_size     = 5
